193835747510e5bf2ebe2f7869bf408d648da2bd
Lucia De Mojà <luciademoja@gmail.com>
2016-07-24 13:07:19 +0200

    tests if number has reminder different than 0

--- spec/fizz_buzz_spec.js
+++ spec/fizz_buzz_spec.js
@@ -8,6 +8,14 @@ describe("FizzBuzz", function() {
   it("Check if number has zero reminder", function() {
       expect(subject.hasZeroReminder(3, 3)).toBe(true);
   });
+
+  it("Check if reminder is different than 0", function() {
+      expect(subject.hasZeroReminder(4, 3)).toBe(false);
+  });
+
+  // it("Check if number has zero reminder", function() {
+  //     expect(subject.hasZeroReminder(3, 3)).toBe(true);
+  // });
 });
 
 //   it("Checks if a given number is divisible by 3", function() {
